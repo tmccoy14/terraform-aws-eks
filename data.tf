85c81e97226cf6a14d932327fb0f4dd4ee181af8
Tucker McCoy <tuckermmccoy@gmail.com>
2021-03-16 20:54:25 -0400

    Added vpc id for cluster

--- data.tf
+++ data.tf
@@ -9,3 +9,24 @@ data "aws_iam_policy_document" "assume_cluster_role" {
     }
   }
 }
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.selected.id
+
+  tags = {
+    "Attributes" = "public"
+  }
+}
+
+data "aws_instances" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+
+  depends_on = [aws_eks_node_group.eks-cluster-nodegroup]
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,8 @@ variable "cluster_name" {
   description = "Name of the cluster."
   type        = string
 }
+
+variable "vpc_id" {
+  description = "The VPC associated with your cluster."
+  type        = string
+}
